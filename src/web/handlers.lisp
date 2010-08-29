72220d124b406773fb87957979bc7ce96183aeb9
hans <hans@4281704c-cde7-0310-8518-8e2dc76b1ff0>
2010-08-29 06:05:06 +0000

    remove own error handler
    git-svn-id: svn://bknr.net/svn/trunk/bknr/web@4598 4281704c-cde7-0310-8518-8e2dc76b1ff0

--- src/web/handlers.lisp
+++ src/web/handlers.lisp
@@ -316,19 +316,11 @@ authorization?"))
 			     (make-hash-table)))
          (*random-state* (make-random-state t)))
     (do-log-request)
-    (handler-bind
-        ((error #'(lambda (e)
-                    (with-http-response (:content-type "text/html; charset=UTF-8"
-                                                       :response +http-internal-server-error+)
-                      (return-from invoke-handler (prog1
-                                                      (with-http-body ()
-                                                        (website-show-error-page *website* e))
-                                                    (do-error-log-request e)))))))
-      (let ((start (get-internal-real-time)))
-        (prog1
-            (handle handler)
-          (let ((duration (- (get-internal-real-time) start)))
-            (note-run-time-for-statistics handler duration)))))))
+    (let ((start (get-internal-real-time)))
+      (prog1
+          (handle handler)
+        (let ((duration (- (get-internal-real-time) start)))
+          (note-run-time-for-statistics handler duration))))))
 
 
 (defmethod handle ((page-handler page-handler))
